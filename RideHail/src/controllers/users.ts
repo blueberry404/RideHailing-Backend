f2bb05bff80454e81a8e7a59049e46a1e393f12f
blueberry404 <developer.anum@gmail.com>
2019-12-09 02:30:43 +0500

    Api name

--- RideHail/src/controllers/users.ts
+++ RideHail/src/controllers/users.ts
@@ -3,3 +3,28 @@ import { Request, Response } from 'express';
 export const getAll = async (request: Request, response: Response) => {
     response.send({ success: 'success' });
 };
+
+export const bookRide = async (request: Request, response: Response) => {
+    response.send({ success: 'success' });
+};
+
+export const cancelRide = async (request: Request, response: Response) => {
+    response.send({ success: 'success' });
+};
+
+export const acceptRide = async (request: Request, response: Response) => {
+    response.send({ success: 'success' });
+};
+
+export const startRide = async (request: Request, response: Response) => {
+    response.send({ success: 'success' });
+};
+
+/*
+Push notifications:
+- find and notify Nearby Drivers
+- cancel ride to other entity
+- notify consumer that ride has been accepted
+
+HOW NOTIFY USERS FOR CHANGING STATE AND LOCATION?
+*/
