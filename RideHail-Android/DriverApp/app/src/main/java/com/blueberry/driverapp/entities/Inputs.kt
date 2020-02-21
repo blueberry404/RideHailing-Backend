d1deb3292a4453df4f7e3b9fb8b7079af3662e35
Anum <anum.amin@tenpearls.com>
2020-02-21 18:42:13 +0500

    WIP - Driver app

--- RideHail-Android/DriverApp/app/src/main/java/com/blueberry/driverapp/entities/Inputs.kt
+++ RideHail-Android/DriverApp/app/src/main/java/com/blueberry/driverapp/entities/Inputs.kt
@@ -2,6 +2,6 @@ package com.blueberry.consumerapp.entities
 
 class BaseResponse constructor(val success:Boolean, val result: Map<String, Any>)
 
-class LoginInput constructor(val email: String, val password: String, val type: String = "Consumer")
+class LoginInput constructor(val email: String, val password: String, val type: String = "Driver")
 
 class ProfileResponse constructor(val success:Boolean, val result: User?)
