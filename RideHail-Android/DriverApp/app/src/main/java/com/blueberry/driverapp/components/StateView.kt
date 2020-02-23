0d6486baee78c9f41e549e263bf3f74d320df43a
blueberry404 <developer.anum@gmail.com>
2020-02-24 01:43:21 +0500

    Drawable WIP

---
+++ RideHail-Android/DriverApp/app/src/main/java/com/blueberry/driverapp/components/StateView.kt
@@ -0,0 +1,57 @@
+package com.blueberry.driverapp.components
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import com.blueberry.driverapp.R
+import com.blueberry.driverapp.constants.DriverState
+
+
+class StateView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
+
+    private var STATE = intArrayOf(R.attr.state)
+    private var mState: DriverState = DriverState.NOT_AVAILABLE
+
+    init {
+        initAttrs(context, attrs)
+    }
+
+    private fun initAttrs(context: Context, attrs: AttributeSet?) {
+        attrs?.let {
+            val attrArr = context.obtainStyledAttributes(attrs, R.styleable.DriverState)
+            try {
+                val state = attrArr.getString(R.styleable.DriverState_state)
+                state?.let {
+                    assignState(it)
+                }
+            }
+            finally {
+                attrArr.recycle()
+            }
+        }
+    }
+
+    fun setState(state: String) {
+        assignState(state)
+        invalidate()
+        requestLayout()
+    }
+
+    private fun assignState(state: String) {
+        mState = when(state) {
+            "NotAvailable" -> DriverState.NOT_AVAILABLE
+            "Idle" -> DriverState.IDLE
+            "Busy" -> DriverState.BUSY
+            else -> throw IllegalStateException("Driver State must be one of the NotAvailable, Busy or Idle")
+        }
+    }
+
+    override fun onCreateDrawableState(extraSpace: Int): IntArray {
+
+        val drawableStates = super.onCreateDrawableState(extraSpace + 1)
+        mergeDrawableStates(drawableStates, STATE)
+        return drawableStates
+    }
+}
---
+++ RideHail-Android/DriverApp/app/src/main/java/com/blueberry/driverapp/constants/enums.kt
@@ -0,0 +1,7 @@
+package com.blueberry.driverapp.constants
+
+enum class DriverState {
+    NOT_AVAILABLE,
+    IDLE,
+    BUSY
+}
