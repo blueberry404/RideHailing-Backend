af5b2daf55c94e252d7382ac4fb5723a4bfd5d3f
blueberry404 <developer.anum@gmail.com>
2020-03-04 06:46:54 +0500

    WIP: Incoming ride request

---
+++ RideHail-Android/DriverApp/app/src/main/java/com/blueberry/driverapp/activities/RideRequestActivity.kt
@@ -0,0 +1,13 @@
+package com.blueberry.driverapp.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.blueberry.driverapp.R
+
+class RideRequestActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ride_request)
+    }
+}
