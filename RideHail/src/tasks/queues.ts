82b1c6eabe34b87af9a0539327a9684adfb373e9
Anum <anum.amin@tenpearls.com>
2020-03-22 02:55:06 +0500

    Redis connection success for queue manager

--- RideHail/src/tasks/queues.ts
+++ RideHail/src/tasks/queues.ts
@@ -1,7 +1,8 @@
 import Queue from 'bull';
+import * as Redis from '../redisClient';
 
 export const FIND_NEARBY_DRIVER_URL = 'FIND_NEARBY_DRIVER_URL';
 
 export const queues = {
-    [FIND_NEARBY_DRIVER_URL]: new Queue(FIND_NEARBY_DRIVER_URL, process.env.REDIS_URL as string)
+    [FIND_NEARBY_DRIVER_URL]: new Queue(FIND_NEARBY_DRIVER_URL, { redis: Redis.getRedisConfig() })
 };
