a9475b42513f00a9bf16eb244945c04ae99fa27e
Anum <anum.amin@tenpearls.com>
2020-03-22 02:55:42 +0500

    postgres connection resolve for another node worker process

--- RideHail/src/tasks/worker.ts
+++ RideHail/src/tasks/worker.ts
@@ -1,7 +1,27 @@
+import { createConnection } from 'typeorm';
+
 import { queues } from './queues';
 import processorInitializers from './processor';
+import { typeOrmConfig } from '../config';
 
-Object.entries(queues).forEach(([queueName, queue]) => {
-    console.log(`Worker listening to '${queueName}' queue`);
-    queue.process(processorInitializers[queueName]);
-  });
+(async () => {
+
+  try {
+    const conn = await createConnection(typeOrmConfig);
+    console.log('Worker:::: PG connected.');
+
+    process.on('exit', async () => {
+      await conn.close();
+      console.log('Worker:::: PG connection closed in queue manager');
+    });
+
+    Object.entries(queues).forEach(([queueName, queue]) => {
+      console.log(`Worker listening to '${queueName}' queue`);
+      queue.process(processorInitializers[queueName]);
+    });
+
+  }
+  catch (ex) {
+    console.error(ex);
+  }
+})();
